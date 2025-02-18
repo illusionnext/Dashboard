bfec7c4c1b4bab10009d893dd886e18e110c1694
Illusion <fargan@st.amu.edu.pl>
2025-02-18 15:43:41 +0100

    implemented NEON postgres DB from integrated in Vercel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+import { randomUUID } from "crypto";
+
 export default function Page() {
+  const uuidLibrary = uuidv4(),
+    uuidNative = crypto.randomUUID();
+
+  console.log("uuid library", uuidLibrary); //This code only work in .log not in .dir
+  console.log("uuid native", uuidNative); //Support only in modern browsers
+  console.log("uuid nodejs", randomUUID()); //Support only in modern browsers Node.js v16.6.0 or later
   return <p>Dashboard Page</p>;
 }
