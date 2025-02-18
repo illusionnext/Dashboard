9ab6f8692f1fa0d67ae4c7d46c307cea3d384e4a
Illusion <fargan@st.amu.edu.pl>
2025-02-18 22:47:39 +0100

    Partial Prerendering

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from "uuid";
+import { randomUUID } from "crypto";
+
+import { Suspense } from "react";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from "@/app/ui/skeletons";
+import CardWrapper from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+
+const Page = async () => {
+  const uuidLibrary = uuidv4(),
+    uuidNative = crypto.randomUUID();
+
+  console.log("uuid library", uuidLibrary); //This code only work in .log not in .dir
+  console.log("uuid native", uuidNative); //Support only in modern browsers
+  console.log("uuid nodejs", randomUUID()); //Support only in modern browsers Node.js v16.6.0 or later
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
